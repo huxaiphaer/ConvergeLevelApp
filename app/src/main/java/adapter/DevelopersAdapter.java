fa3650cb959e27dfbd9bb293bf665f1d9d9c4398
huxaiphaer <l654321123>
2018-11-19 10:30:10 +0300

    feat(details): Creating the Retrofit instance
    - ensure that Retrofit is instantiated.
    
    [Delivers #155890684]

--- app/src/main/java/adapter/DevelopersAdapter.java
+++ app/src/main/java/adapter/DevelopersAdapter.java
@@ -67,7 +67,6 @@ public class DevelopersAdapter extends  RecyclerView.Adapter<DevelopersAdapter.D
         });
     }
 
-
     @Override
     public int getItemCount() {
         return itemList.size();
--- app/src/main/java/service/GetData.java
+++ app/src/main/java/service/GetData.java
@@ -8,6 +8,6 @@ import retrofit2.http.GET;
 
 public interface GetData {
 
-    @GET("/search/users")
+    @GET("search/users?q=type:User+location:Nairobi+language:JAVA")
     Call<List<DeveloperModel>> getDevelopers();
 }
